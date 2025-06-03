53b459e11e63d94d20b2e8612630f7cd8f5cc8f9
tharindi22 <ttharindiattanayaka@gmail.com>
2025-06-03 19:20:55 +0530

    Updated scroll feature

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 
 const Navbar = () => {
+
+  const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef();
 
   const openMenu = () => {
@@ -13,14 +15,25 @@ const Navbar = () => {
     sideMenuRef.current.style.transform = "translate(16rem)";
   };
 
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (scrollY > 50) {
+        setIsScroll(true)
+      }else {
+        setIsScroll(false)
+      }
+    })
+  },[])
+
   return (
     <>
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
         <Image src={assets.header_bg_color} alt="" className="w-full" />
       </div>
       <div>
-        <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
-          <a href="">
+        <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50
+          ${isScroll ? 'bg-white bg-opacity-50 backdrop-blur-lg shadow-sm' : ""}`}>
+          <a href="#top">
             <Image
               src={assets.logo}
               alt=""
@@ -28,21 +41,22 @@ const Navbar = () => {
             />
           </a>
 
-          <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
-            <li className="font-Ovo">
-              <a href="#top">Home</a>
+          <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 
+          ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"}`}>
+            <li >
+              <a className="font-Ovo" href="#top">Home</a>
             </li>
-            <li className="font-Ovo ">
-              <a href="#about">About Me</a>
+            <li>
+              <a className="font-Ovo " href="#about">About Me</a>
             </li>
-            <li className="font-Ovo">
-              <a href="#services">Services</a>
+            <li>
+              <a className="font-Ovo" href="#services">Services</a>
             </li>
-            <li className="font-Ovo">
-              <a href="#work">My Work</a>
+            <li>
+              <a className="font-Ovo" href="#work">My Work</a>
             </li>
-            <li className="font-Ovo">
-              <a href="#contact">Contact Me</a>
+            <li>
+              <a className="font-Ovo" href="#contact">Contact Me</a>
             </li>
           </ul>
 
