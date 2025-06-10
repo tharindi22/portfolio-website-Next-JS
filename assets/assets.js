2f040307a29be7dac55fbb047d721459f8b58e23
tharindi22 <ttharindiattanayaka@gmail.com>
2025-06-10 22:17:31 +0530

    Add some images

--- assets/assets.js
+++ assets/assets.js
@@ -35,6 +35,8 @@ import right_arrow from './right-arrow.png';
 import send_icon from './send-icon.png';
 import right_arrow_bold from './right-arrow-bold.png';
 import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import github from './github.png';
+
 
 export const assets = {
     user_image,
@@ -73,7 +75,8 @@ export const assets = {
     right_arrow,
     send_icon,
     right_arrow_bold,
-    right_arrow_bold_dark
+    right_arrow_bold_dark,
+    github
 };
 
 export const workData = [
